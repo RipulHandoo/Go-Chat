62f5f56871ab470757fd4d40f8be8a945ed9ca8f
Ripul Handoo <ripulhandoo1234@gmail.com>
2023-06-30 00:54:48 +0530

    Log out Server Compeleted

---
+++ handler/auth/LogOut.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/RipulHandoo/goChat/db/database"
+	"github.com/RipulHandoo/goChat/utils"
+)
+
+func LogOut(w http.ResponseWriter, r *http.Request, user database.User){
+	// clear cookies
+	
+	http.SetCookie(w,&http.Cookie{
+		Name: "auth_token",
+		Value:"",
+		Path: "/",
+	})
+
+	utils.ResponseWithJson(w,http.StatusOK,user)
+}
