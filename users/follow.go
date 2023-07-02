0bb6e9dee2a16944f57d2e79c49fa7d8a3a9ece7
Ripul Handoo <ripulhandoo1234@gmail.com>
2023-07-02 16:13:31 +0530

    Follow User Handler

---
+++ users/follow.go
@@ -0,0 +1,60 @@
+package users
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/RipulHandoo/goChat/db"
+	"github.com/RipulHandoo/goChat/db/database"
+	"github.com/RipulHandoo/goChat/utils"
+)
+
+
+func FollowUser(w http.ResponseWriter, req *http.Request, user database.User) {
+	toFollowIDParam := req.URL.Query().Get("id")
+    if toFollowIDParam == "" {
+        utils.ResponseWithError(w, http.StatusBadRequest, fmt.Errorf("can not get the id"))
+        return
+    }
+
+    toFollowID, err := strconv.ParseInt(toFollowIDParam, 10, 64)
+    if err != nil {
+        utils.ResponseWithError(w, http.StatusBadRequest, err)
+        return
+    }
+	apiConfig := db.DbClient
+	userFollowTuple, followerUpdateErr := apiConfig.FollowUser(req.Context(), database.FollowUserParams{
+		FollowingID: toFollowID,
+		FollowerID:  user.ID,
+	})
+	if followerUpdateErr != nil {
+		utils.ResponseWithError(w, http.StatusInternalServerError, followerUpdateErr)
+		return
+	}
+	utils.ResponseWithJson(w, http.StatusOK, userFollowTuple)
+}
+
+func UnFollowUser(w http.ResponseWriter, req *http.Request, user database.User) {
+	toFollowIDParam := req.URL.Query().Get("id")
+    if toFollowIDParam == "" {
+        utils.ResponseWithError(w, http.StatusBadRequest, fmt.Errorf("can not get the id"))
+        return
+    }
+
+    toFollowID, err := strconv.ParseInt(toFollowIDParam, 10, 64)
+    if err != nil {
+        utils.ResponseWithError(w, http.StatusBadRequest, err)
+        return
+    }
+	apiConfig := db.DbClient
+	userFollowTuple, followerUpdateErr := apiConfig.UnfollowUser(req.Context(), database.UnfollowUserParams{
+		FollowingID: toFollowID,
+		FollowerID:  user.ID,
+	})
+	if followerUpdateErr != nil {
+		utils.ResponseWithError(w, http.StatusInternalServerError, followerUpdateErr)
+		return
+	}
+	utils.ResponseWithJson(w, http.StatusOK, userFollowTuple)
+}
